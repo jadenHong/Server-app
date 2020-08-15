1d8f8e7cfb612a672e66d6f8d3cb32316bff8d15
jadenHong <62584142+jadenHong@users.noreply.github.com>
2020-08-15 02:52:52 -0600

    Node js: server-app
    finished login section

--- bulletin-server.js
+++ bulletin-server.js
@@ -3,6 +3,7 @@ console.log('Hong\'s server');
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt'); // hash 로 salting 해서 password를 암호화하여 저장하기 위한 API 호출
 
 const pool = mysql.createPool({
     host: 'localhost',
@@ -10,7 +11,7 @@ const pool = mysql.createPool({
     user: 'root',
     password: 'ap28xm59',
     database: 'bulletin_practice',
-    watiForConnections: true,
+    waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
 });
@@ -98,7 +99,7 @@ const updateData = async (bodyInfo) => {
 }
 // ***************************************************************
 
-// *********** Update clicked data from Database ************
+// *********** Delete clicked data from Database ************
 
 app.get('/movie/deleteData/:id', async (req, res) => {
     const id = req.params.id;
@@ -138,4 +139,89 @@ const getDataByTitle = async (title) => {
     return rows;
 }
 
-// ************************************************************
+// **************************************************************
+
+
+// ************************** Sign In ***************************
+// express 에서 res.json() 한개이상쓰면 에러가 난다 그래서 무조건 res.json()이 두개이상이면 return 을 해줘야한다.
+// 서버측에서 동기로 진행을 하면 hashing 시간이 오래걸려서 퍼포먼스 이슈가 발생할 가능성이 크다. 그래서 비동기방식을 사용하는게 효과적이다. 즉 collback or promise.
+app.post('/movie/login', async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return next(new Error('Invalid request body'));
+    }
+
+    const passwordHash = await getPasswordHashByUsername(username);
+    console.log(passwordHash)
+    if (!passwordHash) {
+        return res.json('Username does not exist');
+    }
+
+    const isAuthenticated = await bcrypt.compare(password, passwordHash);
+    isAuthenticated ? res.json("Loged in successfully") : res.json("Password is not correct");
+
+});
+
+const getPasswordHashByUsername = async (username) => {
+    // 이걸 하시고 넵
+    const QUERY = "SELECT password FROM loginDB where username = ?";
+    const [rows] = await promisePool.query(QUERY, username);
+    return rows[0].password;
+}
+
+
+// **************************************************************
+
+
+
+// ************************** Sign Up ***************************
+
+app.post('/movie/signup', async (req, res, next) => { // next 를 주게 되면 그 다음 미들웨어로 넘어간다.그래서 에러를 핸들링해줄수 있음.
+    const body = req.body;
+    console.log(body.password);
+    const saltRounds = 10; //hash 의 알고리즘이 공개되어있기때문에 취약하다 그래서 salt를 사용하는데 salt는 패스워드마다 생성되는 랜덤값이다. 예를 들어 같은 1234라도 salt가 결합되면 다양한 해시값이 생성된다. 10은 default값이고 이 숫자가 높아지면 높아질수록 (시간을 길게 줄수록) 더 복잡한 해시값을 얻는다.(즉 해시값을 좀 더 복잡하게 암호화 할 수 있음.)
+    const username = body.username;
+    const password = body.password;
+    try {
+        const hash = await bcrypt.hash(password, saltRounds);
+        const usernameAlreadyExists = await usernameDoesExist(username);
+        if (!usernameAlreadyExists) {
+            const result = await storeUserInfo(username, hash);
+            res.json(result);
+        } else {
+            res.send("username is overlapped"); //스트링으로 전송하기 때문에 프론트단에서 fetch후에 response.json() 이 아니라 respose.text()를 해주거나 res.json("username is overlapped") 로 표기해줘야한다!!
+        }
+    } catch (error) {
+        next(new Error(error.message));
+    }
+});
+
+const usernameDoesExist = async (username) => {
+    const QUERY = 'SELECT username FROM loginDB where username = ?';
+    const [rows] = await promisePool.query(QUERY, username);
+    return rows.length > 0
+}
+
+app.use((err, req, res, next) => {
+    //위에 함수에 매칭 안되거나 next()로 걸러진게 여기로 다옴
+    if (err) {
+        // 여기서 err 잡지 않고 에러만 핸들링하는 메들웨어로 보내주는 이유는 플젝이 커지고 코드가 많아질 경우 유지보수를위해!
+        // res.status(500).send(err.message)
+        next();
+    } else {
+        res.status(404).send('route not found');
+    }
+});
+
+app.use((err, req, res, next) => {
+    // 여긴 에러만 핸들링
+    res.status(500).send(err.message);
+})
+
+const storeUserInfo = async (username, password) => {
+    const QUERY = 'INSERT INTO loginDB(username, password, date) VALUES (?,?,current_timestamp())';
+    const [rows] = await promisePool.query(QUERY, [username, password]);
+    return rows;
+}
+
+// **************************************************************
